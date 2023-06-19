59425f55ee14f4357041d0f09efa519617ef4123
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-06-20 01:08:25 +0300

    setup files adding packages and theme Relate #28

---
+++ src/utilize/Theme.jsx
@@ -0,0 +1,35 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ThemeProvider, createTheme } from '@mui/system';
+
+const theme = createTheme({
+  palette: {
+    colors: {
+      primary: '#000',
+      secondary: '#fff',
+      darkBlue: '#002B5B',
+      error: '#EA5455',
+    },
+    bg: {
+      primary: '#F9F5EB',
+      secondary: '#E4DCCF',
+      third: '#EA5455',
+      fourth: '#002B5B',
+    },
+    fontSizes: {
+      small: '0.75rem',
+      md: '1rem',
+      lg: '1.25rem',
+      xl: '10rem',
+    },
+    fontWeight: {
+      sm: 400,
+      md: 500,
+      lg: 600,
+      xl: 700,
+    },
+  },
+});
+
+const Theme = ({ children }) => <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+
+export default Theme;
