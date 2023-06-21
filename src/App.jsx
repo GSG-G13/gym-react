889262ef5d662606db8c046b90591f54d14cf2a9
abeerAyad <abeerayyad2000@gmail.com>
2023-06-21 21:44:31 +0300

    add footer Relates #16 Closes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { Box } from '@mui/material';
 import THEME from './utilize/Theme';
-import Footer from './footer/Footer';
 
 const App = () => (
   <THEME>
     <Box>
-      <Footer />
+      test
     </Box>
   </THEME>
 );
--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -8,38 +8,38 @@ import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 
 const Footer = () => (
 
-  <Box sx={{
-    display: 'flex',
-    alignItems: 'center',
-    backgroundColor: '#E4DCCF',
-    height: '163px',
-    width: '100vw',
-  }}
+  <Box
+    sx={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      backgroundColor: '#E4DCCF',
+      height: '160px',
+
+    }}
   >
-    <Container>
     <Box>
-      <Typography>
+      <Typography variant="h6">
         Gaza, Alrimal, AlJondi St., Capital Mall Building,
         5th Floor, Room 501
       </Typography>
-      <Box>
-        <FacebookIcon />
-        <InstagramIcon />
-        <YouTubeIcon />
-        <LinkedInIcon />
+      <Box mt="15px">
+        <FacebookIcon sx={{ px: '5px' }} />
+        <InstagramIcon sx={{ px: '5px' }} />
+        <YouTubeIcon sx={{ px: '5px' }} />
+        <LinkedInIcon sx={{ px: '5px' }} />
       </Box>
     </Box>
     <Box>
-      <Typography variant="h5">
+      <Typography variant="h6">
         Non Copyrighted © 2023 Design and upload by rich technologies
       </Typography>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', mt: '15px' }}>
         <LocalPhoneIcon />
         <Typography variant="h5">+972 595036287</Typography>
 
       </Box>
     </Box>
-    </Container>
   </Box>
 );
 
