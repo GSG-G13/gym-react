47638d0e71e2575a2125087c6476bd32422a3f6e
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-06-21 09:43:31 +0300

    form dashboard componenet Relate #9

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Box } from '@mui/material';
 import './App.css';
 import THEME from './utilize/Theme';
-import { SideBar } from './components';
+import FormDashBoard from './components/formDashboard/FormDashBoard';
 
 const App = () => (
   <THEME>
     <Box>
-      <SideBar />
+      <FormDashBoard />
     </Box>
   </THEME>
 );
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -5,7 +5,7 @@ const ButtonComponent = ({
   color,
   children,
 }) => (
-  <Button variant={variant} sx={{ backgroundColor: color }}>{children}</Button>
+  <Button variant={variant} sx={{ backgroundColor: color, fontSize: 12 }}>{children}</Button>
 );
 
 export default ButtonComponent;
