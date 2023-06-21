58ce17e6a1ec01e4ca2686df1020644450668603
abeerAyad <abeerayyad2000@gmail.com>
2023-06-21 12:42:31 +0300

    navbar comp Relates #43 Closes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Box } from '@mui/material';
 import './App.css';
 import THEME from './utilize/Theme';
-import { SideBar } from './components';
+import NavbarClient from './components/navbar/NavbarClient';
 
 const App = () => (
   <THEME>
     <Box>
-      <SideBar />
+      <NavbarClient />
     </Box>
   </THEME>
 );
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import MenuComp from './menu/Menu';
+import NavbarClient from './navbar/NavbarClient';
 import SideBar from './sideBar/SideBar';
 
-export { MenuComp, SideBar };
+export { MenuComp, SideBar, NavbarClient };
