4feae30b0eb68b9f56fdbacdce64db998ca1b82f
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-06-21 11:23:40 +0300

    add navbar dashboard Relates #5

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import MenuComp from './menu/Menu';
 import SideBar from './sideBar/SideBar';
+// eslint-disable-next-line import/no-unresolved
+import NavBarDashBoard from './navbarDashboard';
 
-export { MenuComp, SideBar };
+export { MenuComp, SideBar, NavBarDashBoard };
---
+++ src/components/navbarDashboard/index.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import { height } from '@mui/system';
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function NavBarDashBoard() {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar sx={{ width: '100vw', backgroundColor: '#5A67BA' }}>
+      <Container>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            <FitnessCenterIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1, fontSize: 20 }} />
+
+            Gym
+          </Typography>
+
+          <Box sx={{ flexGrow: 0, display: 'flex', alignItems: 'center', gap: 1 }}>
+            <Tooltip title="Open settings" >
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" sx={{ width: 25, height: 25 }} />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+            <Typography variant='h6'>Admin</Typography>
+
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default NavBarDashBoard;
