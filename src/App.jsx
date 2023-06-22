446a63c51adc848f2e985a3d656ca129582c6791
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-06-22 15:32:39 +0300

    add table comp Relates #67

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Box } from '@mui/material';
 import THEME from './utilize/Theme';
+import DataGridDemo from './components/table';
 
 const App = () => (
   <THEME>
     <Box>
-      test
+      <DataGridDemo />
     </Box>
   </THEME>
 );
