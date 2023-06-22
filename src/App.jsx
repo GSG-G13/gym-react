049b245a87489f0f4088c64bb1ae5459af9fc543
AhmedMughessib <amoghaseeb@gmail.com>
2023-06-22 12:48:49 +0300

    delete extra lines in app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { Box } from '@mui/material';
 import THEME from './utilize/Theme';
-import SignInComp from './components/signinform/singinform';
 
 const App = () => (
   <THEME>
     <Box>
-      <SignInComp />
       test
     </Box>
   </THEME>
