a1b95df5f10e62daf80cf7d41e6eca666dad4da8
abeerAyad <abeerayyad2000@gmail.com>
2023-06-24 12:43:24 +0300

    delete component from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { Box } from '@mui/material';
 import THEME from './utilize/Theme';
-import Store from './pages/user/product';
 import './index.css';
 
 const App = () => (
 
   <THEME>
     <Box backgroundColor="colors.primary">
-      <Store />
+      Hello from GYM APP
     </Box>
   </THEME>
 );
