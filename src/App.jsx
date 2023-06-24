196f18bc692c2e16b3286e4347c734f16ba2cd08
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-06-24 13:44:46 +0300

    clean app file Relates #80

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Box } from '@mui/material';
 import THEME from './utilize/Theme';
-import Home from './pages';
 
 const App = () => (
 
   <THEME>
     <Box>
-      <Home />
+      text
     </Box>
   </THEME>
 );
