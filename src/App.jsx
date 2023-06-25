1038e3f5b709058b45b7328de1d4a386e3e60d40
AhmedMughessib <amoghaseeb@gmail.com>
2023-06-25 10:39:03 +0300

    save work

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Box } from '@mui/material';
 import THEME from './utilize/Theme';
+import SignupPage from './pages/user/signup';
 
 const App = () => (
 
   <THEME>
     <Box>
       test
+      <SignupPage />
     </Box>
   </THEME>
 );
---
+++ src/pages/user/signup.jsx
@@ -0,0 +1,11 @@
+import SignupComp from '../../components/signup';
+import NavbarClient from '../../components/navbar/NavbarClient';
+
+const SignupPage = () => (
+  <div>
+    <NavbarClient />
+    {/* <SignupComp /> */}
+  </div>
+);
+
+export default SignupPage;
