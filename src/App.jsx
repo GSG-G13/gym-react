3da6266a13e43ac7446513a64d93dd4b0e47de32
AhmedMughessib <amoghaseeb@gmail.com>
2023-06-25 15:10:20 +0300

    remove extra lines form app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { Box } from '@mui/material';
 import THEME from './utilize/Theme';
-import SignupComp from './components/signup';
 
 const App = () => (
 
   <THEME>
     <Box>
-      <SignupComp />
       Hello from GYM APP
     </Box>
   </THEME>
