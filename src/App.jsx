8afe03cdaca213dbc3e76ea44081b4d03e0fd439
AhmedMughessib <amoghaseeb@gmail.com>
2023-06-26 15:19:13 +0300

    change routerprovider to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/material';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider as Router } from 'react-router-dom';
 import THEME from './utilize/Theme';
 import router from './routes/router';
 import './index.css';
@@ -8,7 +8,7 @@ import Provider from './context/Provider';
 const App = () => (
   <Provider>
     <THEME>
-      <RouterProvider router={router} />
+      <Router router={router} />
       <Box backgroundColor="colors.primary">
         Hello from GYM APP
 
