00ce6812f60d2b4cd74fe6080b6e1ff3837af984
abeerAyad <abeerayyad2000@gmail.com>
2023-06-26 11:13:15 +0300

    remove comp from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { Box } from '@mui/material';
 import THEME from './utilize/Theme';
 import './index.css';
-import { Store } from './pages';
 
 const App = () => (
 
   <THEME>
     <Box backgroundColor="colors.primary">
-      {/* Hello from GYM APP */}
-      <Store />
+      Hello from GYM APP
     </Box>
   </THEME>
 );
