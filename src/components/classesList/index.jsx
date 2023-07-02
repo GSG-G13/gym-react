52a3e87e8b8c727fe899596d7bc3eda24491f35f
abeerAyad <abeerayyad2000@gmail.com>
2023-07-02 20:25:00 +0300

    class functionality Relates #129

--- src/components/classesList/index.jsx
+++ src/components/classesList/index.jsx
@@ -1,28 +1,46 @@
 import { Box, Divider, Typography } from '@mui/material';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
-const classes = ['Yoga', 'Building', 'Fitness', 'food'];
-const ClassesList = () => (
-  <Box width={350} border="1px solid #ccc" mt={15} mb={15} borderRadius={5} p={3}>
-    <Typography pt={15} sx={{ fontSize: '25px', padding: '26px', paddingLeft: '5px' }}>Classes</Typography>
-    <Divider />
-    <Box>
-      {classes.map((classitem) => (
-        <Typography
-          key={classitem}
-          p={2}
-          sx={{
-            backgroundColor: classitem === 'Yoga' ? 'colors.darkBlue' : 'none',
-            color: classitem === 'Yoga' ? '#fff' : '#000',
-            fontSize: '16px',
-          }}
-          mt={3}
-        >
-          {classitem}
-        </Typography>
-      ))}
+// const classes = ['Yoga', 'Building', 'Fitness', 'food'];
+const ClassesList = () => {
+  const [classData, setClassData] = useState([]);
+
+  const getClassData = async () => {
+    const response = await axios.get('/api/classes');
+    setClassData([...classData, response.data.classesData]);
+  };
+
+  useEffect(() => {
+    getClassData();
+  }, []);
+
+  return (
+    <Box width={350} border="1px solid #ccc" mt={15} mb={15} borderRadius={5} py={3} pl={2}>
+      <Typography pt={15} sx={{ fontSize: '25px', padding: '26px', paddingLeft: '5px' }}>Classes</Typography>
+      <Divider />
+      <Box>
+        {classData[0]?.map((classItem) => (
+          <NavLink
+            // eslint-disable-next-line no-underscore-dangle
+            to={`${classItem._id}`}
+            key={classItem.className}
+            style={{
+              backgroundColor: classItem.className === 'Yoga' ? '#002B5B' : 'none',
+              color: classItem.className === 'Yoga' ? '#fff' : '#000',
+              fontSize: '16px',
+              textDecoration: 'none',
+              display: 'block',
+              padding: '16px',
+            }}
+            mt={3}
+          >
+            {classItem.className}
+          </NavLink>
+        ))}
+      </Box>
     </Box>
-  </Box>
-);
-
+  );
+};
 export default ClassesList;
