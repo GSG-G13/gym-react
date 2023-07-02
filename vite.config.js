24c01123b3c34e7aaeffbacdc6abe5e88a9c06b4
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-07-02 15:45:53 +0300

    connection between front and back Relates #125

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5050',
+        changeOrigin: true,
+      },
+    },
+  },
 });
