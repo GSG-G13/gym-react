b82a222deaa498b53b7db929c4899cbdd73c7523
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-07-03 11:52:10 +0300

    get product by categories

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unreachable-loop */
-import * as React from 'react';
 import Box from '@mui/material/Box';
 import { Container, Divider, Typography } from '@mui/material';
+import { useState } from 'react';
 import ProductCard from '../productCard';
 
 const productsobj = [{
@@ -58,31 +58,34 @@ const productsobj = [{
 },
 ];
 
-const ProductList = () => (
-  <Box
-    mb={10}
-    component="div"
-    flex={1}
-    py={3}
-  >
-    <Container>
-      <Typography pb={1} align="left" variant="h3" fontWeight={600}>Clothes</Typography>
-      <Divider />
+const ProductList = () => {
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  return (
+    <Box
+      mb={10}
+      component="div"
+      flex={1}
+      py={3}
+    >
+      <Container>
+        <Typography pb={1} align="left" variant="h3" fontWeight={600}>Clothes</Typography>
+        <Divider />
 
-      <Box
-        px={4}
-        my={5}
-        sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(5, 200px)',
-          gap: 3,
-        }}
-      >
-        {productsobj.map((product) => (
-          <ProductCard key={product.title} product={product} />
-        ))}
-      </Box>
-    </Container>
-  </Box>
-);
+        <Box
+          px={4}
+          my={5}
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(5, 200px)',
+            gap: 3,
+          }}
+        >
+          {productsobj.map((product) => (
+            <ProductCard key={product.title} product={product} />
+          ))}
+        </Box>
+      </Container>
+    </Box>
+  );
+};
 export default ProductList;
