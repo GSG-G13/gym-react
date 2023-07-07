a8fcc98a01c5c84faf48a355ca104b766d5f9ff3
heleena-fadi <miss.heleena98@gmail.com>
2023-07-07 10:30:33 +0300

    styleing store page

--- src/components/homeStore/HomeStore.jsx
+++ src/components/homeStore/HomeStore.jsx
@@ -1,49 +1,49 @@
 import React from 'react';
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 const HomeStore = () => (
-  <Box height="100vh">
-    <Container>
-      <Box display="flex" marginTop="10px" height="700px" justifyContent="space-around">
-        <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column" flex="0.4">
+  <Box
+    height="100vh"
+    sx={{
+      display: 'flex',
+      alignItems: 'center',
+      backgroundImage: 'url(https://static.wixstatic.com/media/11062b_7e91a71d17284e8aaeaa6362a4eb7d92~mv2.jpg/v1/fill/w_640,h_854,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_7e91a71d17284e8aaeaa6362a4eb7d92~mv2.jpg)',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      objectFit: 'cover',
 
-          <Typography fontWeight={600} variant="h1" color="#FF4601">GYM Store</Typography>
-          <Typography mt={3} variant="h5" sx={{ lineHeight: '1.5', letterSpacing: '1.5px', pb: '50px' }}>
-            At Gym, you can shop for all useful stuff , clothes,
-            accessories, tools and more at a single place.
+    }}
+  >
 
-          </Typography>
+    <Box ml="54px" display="flex" alignItems="center" justifyContent="center" flexDirection="column" border="1px solid red" flex={0.45}>
 
-          <div
-            className="sec-store"
-            style={{
-              color: '#FF4601',
-              borderBottomRightRadius: '50%',
-              borderBottomLeftRadius: '50%',
-              backgroundColor: 'white',
-              width: '80px',
-              height: '95px',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              fontSize: '30px',
-              fontWeight: '500',
+      <Typography fontWeight={600} variant="h1" color="#FF4601">GYM Store</Typography>
+      <Typography mt={3} variant="h5" sx={{ lineHeight: '1.5', letterSpacing: '1.5px', pb: '50px' }}>
+        At Gym, you can shop for all useful stuff , clothes,
+        accessories, tools and more at a single place.
 
-            }}
-          >
-            50%
-          </div>
-        </Box>
-        <Box flex="0.6">
-          <img
-            src="../../src/assets/My_project.png"
-            alt="store-pic"
-            width="100%"
-          />
+      </Typography>
 
-        </Box>
-      </Box>
-    </Container>
+      <div
+        className="sec-store"
+        style={{
+          color: '#FF4601',
+          borderBottomRightRadius: '50%',
+          borderBottomLeftRadius: '50%',
+          backgroundColor: 'white',
+          width: '70px',
+          height: '85px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: '20px',
+          fontWeight: '500',
+
+        }}
+      >
+        50%
+      </div>
+    </Box>
   </Box>
 
 );
