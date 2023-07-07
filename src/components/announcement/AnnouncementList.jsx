7e53a559470f3b50579fa0925024fba479b1044e
abeerAyad <abeerayyad2000@gmail.com>
2023-07-07 19:03:41 +0300

    announcement styling Relates #186

--- src/components/announcement/AnnouncementList.jsx
+++ src/components/announcement/AnnouncementList.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Announcement from './Announcement';
@@ -15,8 +15,24 @@ const AnnouncementList = () => {
   }, []);
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} my={10}>
-      {announcement?.map((announce) => <Announcement announce={announce} />)}
+    <Box my={15}>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box sx={{
+          borderWidth: '10px 10px',
+          borderColor: '#ff4601 transparent #ff4601 #ff4601 ',
+          borderStyle: 'solid',
+          pl: '50px',
+        }}
+        />
+        <Typography variant="h2" sx={{ color: '#ff4601', fontWeight: '700', ml: '10px' }}>Announcements</Typography>
+      </Box>
+
+      <Box sx={{
+        display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 2,
+      }}
+      >
+        {announcement?.map((announce) => <Announcement announce={announce} />)}
+      </Box>
     </Box>
   );
 };
