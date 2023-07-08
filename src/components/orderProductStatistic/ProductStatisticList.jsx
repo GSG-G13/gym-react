3cc87a3bc9cec086673ed60ff349fcb1d465ab5b
abeerAyad <abeerayyad2000@gmail.com>
2023-07-08 23:45:20 +0300

    get statistic order and rating chart Relates #197

--- src/components/orderProductStatistic/ProductStatisticList.jsx
+++ src/components/orderProductStatistic/ProductStatisticList.jsx
@@ -1,34 +1,40 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import { useOutletContext } from 'react-router-dom';
 import OrderProductStatistic from './OrderProductStatistic';
 
-const products = ['One', 'Two', 'Three', 'One', 'One', 'Two', 'Three', 'One'];
-const ProductStatisticList = () => (
-  <Box
-    border="1px solid #ccc"
-    p="15px 20px"
-    height="400px"
-    sx={{
-      '&::-webkit-scrollbar': { display: 'none' },
-      overflowY: 'scroll',
-    }}
-  >
-    <Box mb="5px">
-      <Typography variant="h5" fontWeight="700" mb="3px">Most Ordered Product</Typography>
-      <Typography variant="body2" sx={{ color: '#999' }}>which are the most ordered products</Typography>
-    </Box>
-    <Box sx={{
-      display: 'flex',
-      flexDirection: 'column',
-    }}
+const ProductStatisticList = () => {
+  const [users, classes, subs, products, categories, trainers, ordersData] = useOutletContext();
+
+  return (
+    <Box
+      border="1px solid #ccc"
+      p="15px 20px"
+      height="400px"
+      sx={{
+        '&::-webkit-scrollbar': { display: 'none' },
+        overflowY: 'scroll',
+      }}
     >
-      {products.map((product) => (
-        <OrderProductStatistic
-          product={product}
-        />
-      ))}
+      <Box mb="5px">
+        <Typography variant="h5" fontWeight="700" mb="3px">Most Ordered Product</Typography>
+        <Typography variant="body2" sx={{ color: '#999' }}>which are the most ordered products</Typography>
+      </Box>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+      >
+        {products?.map((product) => (
+          <OrderProductStatistic
+            product={product}
+            orders={ordersData}
+          />
+        ))}
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default ProductStatisticList;
