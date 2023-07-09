7a8d8d78ed43efd40338e2d831e46613dae8f784
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-07-09 19:47:40 +0300

    add structure comment box and add comment data Relates #204

--- src/components/announcement/Announcement.jsx
+++ src/components/announcement/Announcement.jsx
@@ -1,24 +1,18 @@
 /* eslint-disable no-underscore-dangle */
 import {
-  Box, Divider, Typography, Button,
+  Box, Divider, Typography, Button, Input,
 } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import PersonIcon from '@mui/icons-material/Person';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
 import Image from '../../styles/image';
 
 const Announcement = ({ announce }) => {
   const [commentsData, setComments] = useState([]);
   const [commentInput, setCommentInput] = useState('');
-
-  const addComment = async () => {
-    try {
-      await axios.post(`/api/comments/${announce._id}`, { comment: commentInput });
-      setCommentInput('');
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const [showComments, setShowComments] = useState(false);
 
   const getComments = async () => {
     try {
@@ -32,6 +26,16 @@ const Announcement = ({ announce }) => {
   const deleteComment = async (id) => {
     try {
       axios.delete(`/api/comments/${id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addComment = async () => {
+    try {
+      await axios.post(`/api/comments/${announce._id}`, { comment: commentInput });
+      setCommentInput('');
+      getComments();
     } catch (error) {
       console.log(error);
     }
@@ -73,29 +77,143 @@ const Announcement = ({ announce }) => {
       <Divider />
 
       <Box p="10px">
-        <Typography mt={1} variant="h4" color="#ff4601" fontWeight={600}>{announce.title}</Typography>
+        <Typography mt={1} variant="h4" fontWeight={600}>{announce.title}</Typography>
         <Typography mt={2} mb={2} color="#fff" variant="body2" maxWidth="650px" width="100%">{announce.description}</Typography>
         {announce.image
-      && (
-      <Box sx={{ height: '350px' }}>
-        <Image src={announce.image} alt="gym" width="100%" height="100%" />
+          && (
+            <Box sx={{ height: '350px' }}>
+              <Image src={announce.image} alt="gym" width="100%" height="100%" />
 
-      </Box>
-      )}
-        <Box>
-          <input type="text" value={commentInput} onChange={(e) => setCommentInput(e.target.value)} />
-          <Button onClick={addComment}>comment</Button>
+            </Box>
+          )}
+        <Box position="relative">
+          <Button
+            sx={{
+              color: 'colors.darkBlue',
+            }}
+            onClick={() => setShowComments(!showComments)}
+          >
+            Comments
+
+          </Button>
+          <Typography sx={{
+            backgroundColor: 'blue',
+            width: '18px',
+            height: '18px',
+            position: 'absolute',
+            borderRadius: '50%',
+            left: 92,
+            top: 0,
+            fontSize: 10,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          >
+            {commentsData.filter((comments) => (comments.announcementId._id === announce._id))
+              .length}
+
+          </Typography>
         </Box>
+
         {commentsData && (
-        <Box>
-          {commentsData.filter((comments) => (comments.announcementId._id === announce._id))
-            .map((comment) => (
-              <Box>
-                <Box>{comment.comment}</Box>
-                <Button onClick={() => deleteComment(comment._id)}>x</Button>
-              </Box>
-            ))}
-        </Box>
+          <Box px={10} sx={{ display: showComments ? 'block' : 'none' }}>
+            {commentsData.filter((comments) => (comments.announcementId._id === announce._id))
+              .map((comment) => (
+                <Box
+                  mt={2}
+                  py={2}
+                  borderBottom="1px solid #ccc"
+                  key={comment._id}
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    gap: 2,
+                  }}
+                  >
+                    <Typography
+                      sx={{
+                        fontSize: 12,
+                      }}
+                    >
+                      {comment.userId.username}
+
+                    </Typography>
+                    <Typography
+                      sx={{
+                        fontSize: 12,
+
+                      }}
+                      variant="h4"
+                    >
+                      {comment.comment}
+
+                    </Typography>
+
+                  </Box>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      gap: 1,
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        color: '#ccc',
+                        fontSize: 10,
+                      }}
+                      variant="h6"
+                    >
+                      {comment.createdAt.split(':')[0].slice(0, 10).replaceAll('-', '/')}
+
+                    </Typography>
+
+                    <DeleteIcon mt={2} sx={{ fontSize: 18, '&:hover': { color: 'red', cursor: 'pointer' } }} onClick={() => deleteComment(comment._id)} />
+
+                  </Box>
+
+                </Box>
+              ))}
+            <Box sx={{
+              display: 'flex',
+              alignItems: 'center',
+              mt: 2,
+            }}
+            >
+              <Input
+                sx={{
+                  color: '#fff',
+                  fontSize: 12,
+                  flex: 0.9,
+                  border: '1px solid #ccc',
+                  p: '5px 10px',
+
+                }}
+                type="text"
+                value={commentInput}
+                onChange={(e) => setCommentInput(e.target.value)}
+              />
+
+              <SendIcon
+                sx={{
+                  color: '#fff',
+                  fontSize: 20,
+                  flex: 0.1,
+                }}
+                onClick={addComment}
+              />
+
+            </Box>
+          </Box>
         )}
       </Box>
     </Box>
