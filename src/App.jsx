71847895898f0a0866603542b882393da570dcb3
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-07-10 14:06:49 +0300

    get trainers home page Relates #212

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,19 @@ import { Box } from '@mui/material';
 import THEME from './utilize/Theme';
 import Provider from './context/Provider';
 import router from './routes/router';
+import useAuth from './hook/useAuth';
 
 const App = () => {
-  const userData = JSON.parse(localStorage.getItem('userData'));
+  const { user, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Box sx={{
-      backgroundColor: userData?.role === 'admin' ? '#fff' : '#1E1E1E',
-      color: userData?.role === 'admin' ? '#000' : '#fff',
+      backgroundColor: user?.role === 'admin' ? '#fff' : '#1E1E1E',
+      color: user?.role === 'admin' ? '#000' : '#fff',
 
     }}
     >
