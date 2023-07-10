9a8cbdff9f842726abac70e0722455e9dacb1019
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-07-10 14:10:59 +0300

    fix styling background based on user role Relates #212

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,15 @@ import { Box } from '@mui/material';
 import THEME from './utilize/Theme';
 import Provider from './context/Provider';
 import router from './routes/router';
-import useAuth from './hook/useAuth';
 
-const App = () => {
-  const { user, isLoading } = useAuth();
+const App = () => (
+  <Box>
+    <Provider>
+      <THEME>
+        <RouterProvider router={router} />
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <Box sx={{
-      backgroundColor: user?.role === 'admin' ? '#fff' : '#1E1E1E',
-      color: user?.role === 'admin' ? '#000' : '#fff',
-
-    }}
-    >
-      <Provider>
-        <THEME>
-          <RouterProvider router={router} />
-
-        </THEME>
-      </Provider>
-    </Box>
-  );
-};
+      </THEME>
+    </Provider>
+  </Box>
+);
 export default App;
