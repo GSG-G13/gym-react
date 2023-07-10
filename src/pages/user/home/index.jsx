f3fcb9d9c0d3388d06bd6126537689639c064624
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-07-10 14:15:59 +0300

    get trainers data Relates #212

--- src/pages/user/home/index.jsx
+++ src/pages/user/home/index.jsx
@@ -7,13 +7,6 @@ import { SliderComp } from '../../../components';
 import SwiperComp from '../../../components/swiper';
 import VideoPlayer from '../../../components/videoCom';
 
-const images = ['https://betterbody.ie/wp-content/uploads/elementor/thumbs/strong-man-training-in-gym-1-scaled-puc6dnltxn54i7tqe8yfryh0a4wn9r8z3ewgicqsqo.jpg',
-  'https://betterbody.ie/wp-content/uploads/elementor/thumbs/strong-man-training-in-gym-1-scaled-puc6dnltxn54i7tqe8yfryh0a4wn9r8z3ewgicqsqo.jpg',
-  'https://betterbody.ie/wp-content/uploads/elementor/thumbs/strong-man-training-in-gym-1-scaled-puc6dnltxn54i7tqe8yfryh0a4wn9r8z3ewgicqsqo.jpg',
-  'https://betterbody.ie/wp-content/uploads/elementor/thumbs/strong-man-training-in-gym-1-scaled-puc6dnltxn54i7tqe8yfryh0a4wn9r8z3ewgicqsqo.jpg',
-  'https://betterbody.ie/wp-content/uploads/elementor/thumbs/strong-man-training-in-gym-1-scaled-puc6dnltxn54i7tqe8yfryh0a4wn9r8z3ewgicqsqo.jpg',
-  'https://betterbody.ie/wp-content/uploads/elementor/thumbs/strong-man-training-in-gym-1-scaled-puc6dnltxn54i7tqe8yfryh0a4wn9r8z3ewgicqsqo.jpg'];
-
 const Home = () => (
   <Box>
     <Header />
@@ -29,7 +22,7 @@ const Home = () => (
           <VideoPlayer />
         </Box>
         <Box mb={10} position="relative">
-          <SwiperComp images={images} />
+          <SwiperComp />
         </Box>
       </Box>
     </Container>
