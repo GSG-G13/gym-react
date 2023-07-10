9f6c9ca7db3d77bebafa30e966faf94e65272e88
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-07-10 22:04:41 +0300

    user reset his account password Relates #216

---
+++ src/components/productDetailsCard/style.js
@@ -0,0 +1,13 @@
+const cardStyle = {
+  boxShadow: '-3px 4px 1px -1px #ff4601',
+  display: 'flex',
+  alignItems: 'center',
+  borderRadius: '10px',
+  width: '900px',
+  p: '30px',
+  margin: 'auto',
+  backgroundColor: 'rgba(0,0,0,0.1)',
+
+};
+
+export default cardStyle;
--- src/pages/user/signin/index.jsx
+++ src/pages/user/signin/index.jsx
@@ -1,18 +1,38 @@
 import { Box, Container } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { SignInComp } from '../../../components';
 import { ReactComponent as Login } from '../../../assets/login.svg';
+import ForgetPasswordComp from '../../../components/passwordFrom';
 
-const SignIn = () => (
-  <Box>
-    <Container>
-      <Box sx={{ display: 'flex', alignItems: 'center', height: '100vh' }}>
-        <Login style={{ width: '50%' }} />
+const SignIn = () => {
+  const [showBox, setShowBox] = useState(false);
+  return (
+    <Box>
+      <Container>
+        <Box sx={{ display: 'flex', alignItems: 'center', height: '100vh' }}>
+          <Login style={{ width: '50%' }} />
 
-        <SignInComp />
+          <SignInComp setShowBox={setShowBox} showBox={showBox} />
+        </Box>
+      </Container>
+      <Box
+        display={showBox ? 'block' : 'none'}
+        border="1px solid red"
+        sx={{
+          width: '60%',
+          m: 'auto',
+          height: 500,
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%,-50%)',
+          backgroundColor: 'rgba(0,0,0,0.7)',
+        }}
+      >
+        <ForgetPasswordComp setShowBox={setShowBox} showBox={showBox} />
       </Box>
-    </Container>
-  </Box>
-);
+    </Box>
+  );
+};
 
 export default SignIn;
