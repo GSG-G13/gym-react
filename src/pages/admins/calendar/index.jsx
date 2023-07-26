825cfa6fd7da64f7bee17eb9cae720284c04e755
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-07-26 15:04:38 +0300

    fix get method error when move from month to day and click more Relates #242

--- src/pages/admins/calendar/index.jsx
+++ src/pages/admins/calendar/index.jsx
@@ -32,15 +32,6 @@ const CalendarDash = () => {
   const [classes, setClasses] = useState([]);
   const [classId, setClassId] = useState('');
 
-  const addCalendar = async () => {
-    try {
-      axios.post(`/api/calendar/${classId}`, newDate);
-      setCalendarsData([...calendarsData, newDate]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   const getClasses = async () => {
     try {
       const { data: { classesData } } = await axios.get('/api/classes');
@@ -54,10 +45,10 @@ const CalendarDash = () => {
     try {
       const { data: { calendars } } = await axios.get('/api/calendar');
       const classCalendar = calendars.map((calendarClassData) => ({
-        _id: calendarClassData._id,
-        title: calendarClassData.classId.className,
-        start: calendarClassData.start,
-        end: calendarClassData.end,
+        _id: calendarClassData?._id,
+        title: calendarClassData.classId?.className,
+        start: new Date(calendarClassData?.start),
+        end: new Date(calendarClassData?.end),
       }));
       setCalendarsData(classCalendar);
     } catch (error) {
@@ -65,6 +56,15 @@ const CalendarDash = () => {
     }
   };
 
+  const addCalendar = async () => {
+    try {
+      axios.post(`/api/calendar/${classId}`, newDate);
+      setCalendarsData([...calendarsData, newDate]);
+      getCalendars();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
     getClasses();
     getCalendars();
@@ -72,48 +72,68 @@ const CalendarDash = () => {
 
   return (
     <Box>
-      <FormControl>
-        <InputLabel sx={{ fontSize: 14, color: '#fff' }} htmlFor="class-select">
-          Classes
-        </InputLabel>
+      <Box sx={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(1,1fr)',
+        justifyContent: 'center',
+        gap: 2,
+        mt: 5,
+      }}
+      >
+        <FormControl>
+          <InputLabel sx={{ fontSize: 13, color: '#fff' }} htmlFor="class-select">
+            Classes
+          </InputLabel>
 
-        <Select
-          id="class-select"
-          onChange={(e) => setClassId(e.target.value)}
-          sx={{
-            fontSize: 12,
-            color: '#fff',
-            border: '1px solid #ccc',
-            paddingX: '20px',
-          }}
+          <Select
+            id="class-select"
+            onChange={(e) => setClassId(e.target.value)}
+            sx={{
+              fontSize: 12,
+              color: '#fff',
+              border: '1px solid #373737',
+              width: '100%',
+            }}
+          >
+            {classes.map((classData) => (
+              <MenuItem value={classData?._id} key={classData?._id}>
+                {classData?.className}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: 2,
+          p: 2,
+        }}
         >
-          {classes.map((classData) => (
-            <MenuItem value={classData?._id} key={classData?._id}>
-              {classData?.className}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <DatePicker
-        placeholderText="Start Date"
-        style={{ marginRight: '10px' }}
-        selected={newDate.start}
-        onChange={(start) => setNewDate({ ...newDate, start })}
-      />
-      <DatePicker
-        placeholderText="End Date"
-        selected={newDate.end}
-        onChange={(end) => setNewDate({ ...newDate, end })}
-      />
-      <Button sx={{ marginTop: '10px' }} onClick={addCalendar}>
-        Add Date
-      </Button>
+          <DatePicker
+            placeholderText="Start Date"
+            selected={newDate.start}
+            onChange={(start) => setNewDate({ ...newDate, start })}
+          />
+          <DatePicker
+            placeholderText="End Date"
+            selected={newDate.end}
+            onChange={(end) => setNewDate({ ...newDate, end })}
+          />
+        </Box>
+        <Button sx={{ margin: 'auto', width: 'fit-content' }} onClick={addCalendar}>
+          Add Date
+        </Button>
+      </Box>
       <Calendar
         localizer={localizer}
         events={calendarsData}
         startAccessor="start"
         endAccessor="end"
-        style={{ height: 500, margin: '50px', color: '#fff' }}
+        style={{
+          height: 500, margin: '50px', color: '#fff', fontSize: 12,
+        }}
       />
     </Box>
   );
